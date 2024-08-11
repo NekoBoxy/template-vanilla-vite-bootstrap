1d13241d9b137476ad0aa5a73428af490066fa0a
CatBoxy <catboxy@gmail.com>
2024-08-11 22:05:21 +0800

    #1 模組初建 - vanilla + bootstrap5 + vite & modal

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# template-vanilla-vite-bootstrap
+# vanilla + vite + bootstrap 模板 by CatBoxy
+- 因 sass 1.77.8 有 bug，降板至 1.77.6
+
+## modules - dependencies
+- "@popperjs/core": "^2.11.8",
+- "bootstrap": "^5.3.3"
+
+## modules - devDependencies
+- "sass": "^1.77.6",
+- "vite": "^5.4.0"
---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+import '../scss/styles.scss'
+
+// 法 1
+// import * as bootstrap from 'bootstrap';
+
+// const modelCtrl = function () {
+//   const modalEl = new bootstrap.Modal(document.querySelector("#exampleModal"), {});;
+//   modalEl.show();
+// };
+
+// 法 2 - 只引入需要的功能: Modal
+// import * as bootstrap from 'bootstrap'; // 看需求決定是否引入這行
+import { Modal } from 'bootstrap';
+
+const modelCtrl = function () {
+  const modalEl = new Modal(document.querySelector("#exampleModal"));
+  modalEl.show();
+};
+
+// 法 3 - 與法 2 相同，差在引入的路徑較完整
+// import * as bootstrap from 'bootstrap'; // 看需求決定是否引入這行
+// import Modal from 'bootstrap/js/dist/modal';
+
+// const modelCtrl = function () {
+//   const modalEl = new Modal(document.querySelector("#exampleModal"));
+//   modalEl.show();
+// };
+
+modelCtrl();
