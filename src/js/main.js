cdb0b0e62d0b05de51357a8f04ab5b470d4d1b57
CatBoxy <catboxy@gmail.com>
2024-08-15 20:30:12 +0800

    修改 bootstrap primary 預設顏色

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import '../scss/styles.scss'
+import '../scss/main.scss';
 
 // 法 1
-// import * as bootstrap from 'bootstrap';
+import * as bootstrap from 'bootstrap';
 
 // const modelCtrl = function () {
 //   const modalEl = new bootstrap.Modal(document.querySelector("#exampleModal"), {});;
@@ -10,12 +10,12 @@ import '../scss/styles.scss'
 
 // 法 2 - 只引入需要的功能: Modal
 // import * as bootstrap from 'bootstrap'; // 看需求決定是否引入這行
-import { Modal } from 'bootstrap';
+// import { Modal } from 'bootstrap';
 
-const modelCtrl = function () {
-  const modalEl = new Modal(document.querySelector("#exampleModal"));
-  modalEl.show();
-};
+// const modelCtrl = function () {
+//   const modalEl = new Modal(document.querySelector("#exampleModal"));
+//   modalEl.show();
+// };
 
 // 法 3 - 與法 2 相同，差在引入的路徑較完整
 // import * as bootstrap from 'bootstrap'; // 看需求決定是否引入這行
@@ -26,4 +26,4 @@ const modelCtrl = function () {
 //   modalEl.show();
 // };
 
-modelCtrl();
+// modelCtrl();
