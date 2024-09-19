64562a4142bc2df24ff98f75c927163246845fd0
CatBoxy <catboxy@gmail.com>
2024-09-19 10:56:44 +0800

    移除贅字

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,20 @@
 import '../scss/styles.scss'
 
 // 法 1
-// import * as bootstrap from 'bootstrap';
-
-// const modelCtrl = function () {
-//   const modalEl = new bootstrap.Modal(document.querySelector("#exampleModal"), {});;
-//   modalEl.show();
-// };
-
-// 法 2 - 只引入需要的功能: Modal
-import { Modal } from 'bootstrap';
+import * as bootstrap from 'bootstrap';
 
 const modelCtrl = function () {
-  const modalEl = new Modal(document.querySelector("#exampleModal"));
+  const modalEl = new bootstrap.Modal(document.querySelector("#exampleModal"), {});
   modalEl.show();
 };
+
+// 法 2 - 只引入需要的功能: Modal
+// import { Modal } from 'bootstrap';
+
+// const modelCtrl = function () {
+//   const modalEl = new Modal(document.querySelector("#exampleModal"));
+//   modalEl.show();
+// };
 
 // 法 3 - 極不建議使用。若官方換檔名/結構就全滅
 // import Modal from 'bootstrap/js/dist/modal';
